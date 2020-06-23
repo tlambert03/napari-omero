a83c952cc60afb113f94372cb787d495d27a9256
Talley Lambert <talley.lambert@gmail.com>
2020-06-23 17:00:47 -0400

    add console_script entrypoint

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 
-with open("README.rst", "r") as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
@@ -9,12 +9,16 @@ setup(
     version="0.1.0",
     description="OMERO/napari interoperability",
     long_description=long_description,
+    long_description_content_type='text/markdown',
     author="The Open Microscopy and napari teams",
     packages=["omero_napari", "omero.plugins"],
     package_dir={"": "src"},
     install_requires=["napari[all]>=0.3.0", "omero-py"],
     keywords=["OMERO.CLI", "plugin", "napari"],
-    entry_points={"napari.plugin": ["omero = omero_napari.plugins.napari"]},
+    entry_points={
+        "napari.plugin": ["omero = omero_napari.plugins.napari"],
+        'console_scripts': ["omero_napari = omero_napari.__main__"]
+    },
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Plugins",
