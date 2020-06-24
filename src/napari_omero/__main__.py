8049974fae3f60e357a87769b32abd5887f918cd
Talley Lambert <talley.lambert@gmail.com>
2020-06-24 16:32:28 -0400

    Setup CI (#6)
    * merge in rename refactor
    
    * basic setup
    
    * try installing conda
    
    * add shell again
    
    * try fix coverage
    
    * add deploy
    
    * remove isolated build
    
    * skip windows for now
    
    * deploy only on master

--- src/napari_omero/__main__.py
+++ src/napari_omero/__main__.py
@@ -3,9 +3,18 @@ from qtpy.QtCore import Qt
 
 from .widgets import OMEROWidget
 
-with gui_qt():
-    viewer = Viewer()
-    m = OMEROWidget()
-    dw = viewer.window.add_dock_widget(m, area="right")
-    viewer.window._qt_window.setGeometry(300, 200, 1280, 720)
-    viewer.window._qt_window.resizeDocks([dw], [390], Qt.Horizontal)
+
+def main():
+    with gui_qt():
+        viewer = Viewer()
+        m = OMEROWidget()
+        dw = viewer.window.add_dock_widget(m, area="right")
+        # TODO: figure out dynamic geometry
+        viewer.window._qt_window.setGeometry(300, 200, 1280, 720)
+        viewer.window._qt_window.resizeDocks([dw], [390], Qt.Horizontal)
+
+
+if __name__ == '__main__':
+    import sys
+
+    sys.exit(main())
