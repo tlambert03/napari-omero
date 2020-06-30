01e75075399a16fa68902810b4915e9220bdabea
William Moore <w.moore@dundee.ac.uk>
2020-06-30 10:49:16 +0100

    Add types and save_labels() returns Roi list

--- src/napari_omero/plugins/masks.py
+++ src/napari_omero/plugins/masks.py
@@ -1,11 +1,13 @@
+from typing import List
 import numpy as np
 from random import randint
 
+from omero.gateway import ImageWrapper
 from omero.model import RoiI
 from omero_rois import mask_from_binary_image
 
 
-def create_roi(image, shapes):
+def create_roi(image: ImageWrapper, shapes) -> RoiI:
     updateService = image._conn.getUpdateService()
     roi = RoiI()
     # use the omero.model.ImageI that underlies the 'image' wrapper
@@ -16,7 +18,7 @@ def create_roi(image, shapes):
     return updateService.saveAndReturnObject(roi)
 
 
-def save_labels(masks_4d, image):
+def save_labels(masks_4d: np.ndarray, image: ImageWrapper) -> List[RoiI]:
     """
     Saves masks from a 5D image (no C dimension)
 
@@ -26,13 +28,16 @@ def save_labels(masks_4d, image):
     the the mask.
     """
     # for each label value, check if we have any masks
+    rois = []
     for v in range(1, masks_4d.max() + 1):
         hits = masks_4d.flatten() == v
         if np.any(hits):
-            save_label(masks_4d == v, image)
+            rois.append(save_label(masks_4d == v, image))
+    return rois
 
 
-def save_label(bool_4d, image):
+def save_label(bool_4d: np.ndarray, image: ImageWrapper) -> RoiI:
+    """Turns a boolean array of shape (t, z, y, x) into OMERO Roi"""
     size_t = bool_4d.shape[0]
     size_z = bool_4d.shape[1]
     # Create an ROI with a shape for each Z/T that has some mask
@@ -46,4 +51,4 @@ def save_label(bool_4d, image):
                 mask_shapes.append(mask)
 
     print(f'Creating ROI with {len(mask_shapes)} shapes')
-    create_roi(image, mask_shapes)
+    return create_roi(image, mask_shapes)
