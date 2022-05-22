258da59449f2df14cc873525412ec27833e74cb2
Talley Lambert <talley.lambert@gmail.com>
2022-05-22 18:12:13 -0400

    add test

---
+++ tests/test_widget.py
@@ -0,0 +1,11 @@
+from napari_omero import OMEROWidget
+from unittest.mock import patch
+
+
+def test_widget(make_napari_viewer):
+    from napari_omero.widgets.gateway import QGateWay
+
+    viewer = make_napari_viewer()
+    with patch.object(QGateWay, '_try_restore_session', lambda x: None):
+        widget = OMEROWidget()
+        viewer.window.add_dock_widget(widget)
