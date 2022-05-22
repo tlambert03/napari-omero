85c9ff8d17961d48aac25d76af3b65f549fdda54
Talley Lambert <talley.lambert@gmail.com>
2022-05-22 17:28:17 -0400

    updates

--- src/napari_omero/__main__.py
+++ src/napari_omero/__main__.py
@@ -1,16 +1,12 @@
-import napari
-from qtpy.QtCore import Qt
-
-from .widgets import OMEROWidget
-
-
 def main():
+    from qtpy.QtCore import Qt
+    import napari
 
     viewer = napari.Viewer()
-    m = OMEROWidget()
-    dw = viewer.window.add_dock_widget(m, area="right")
+    dw, _ = viewer.window.add_plugin_dock_widget(
+        'napari-omero', 'OMERO Browser'
+    )
     # TODO: figure out dynamic geometry
-    viewer.window._qt_window.setGeometry(300, 200, 1280, 720)
     viewer.window._qt_window.resizeDocks([dw], [390], Qt.Horizontal)
     napari.run()
 
