8e77e14b39a272060e5f69eff899f556f02d4fc6
Johannes Soltwedel <38459088+jo-mueller@users.noreply.github.com>
2025-03-27 13:05:44 +0100

    added basic widget test

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,9 +1,18 @@
 from unittest.mock import patch
 
 
-def test_widget(make_napari_viewer):
+def test_browser_widget(make_napari_viewer):
     from napari_omero.widgets.gateway import QGateWay
 
     viewer = make_napari_viewer()
     with patch.object(QGateWay, "_try_restore_session", lambda x: None):
         viewer.window.add_plugin_dock_widget("napari-omero", "OMERO Browser")
+
+def test_ROI_uploader_widget(make_napari_viewer):
+    from napari_omero.widgets.gateway import QGateWay
+
+    viewer = make_napari_viewer()
+    with patch.object(QGateWay, "_try_restore_session", lambda x: None):
+        viewer.window.add_plugin_dock_widget("napari-omero", "OMERO ROI Uploader")
+
+
