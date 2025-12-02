eb02b58ddc9dd7abe3f77ec6c704c1d94cb2a482
Peter Sobolewski <76622105+psobolewskiPhD@users.noreply.github.com>
2025-12-02 03:19:34 -0500

    Refactor load/upload ROI widget (#129)
    * Refactor load/upload ROI widget
    
    * style(pre-commit.ci): auto fixes [...]
    
    * Ensure combobox is hooked up properly by using create_widget
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/napari_omero/widgets/ROIs.py
+++ src/napari_omero/widgets/ROIs.py
@@ -1,28 +1,31 @@
 import warnings
 
 import napari.viewer
-from magicgui import magic_factory
-from magicgui.widgets import PushButton
+from magicgui.widgets import Container, PushButton, create_widget
 from napari.layers import Image, Labels
 from napari.utils.notifications import show_info
 
 from napari_omero.plugins.loaders import load_rois
 from napari_omero.plugins.omero import save_rois
 from napari_omero.utils import lookup_obj
+from napari_omero.widgets.gateway import QGateWay
 from omero.cli import ProxyStringType
 
-from .gateway import QGateWay
 
+def omero_roi_manager() -> Container:
+    """A widget to manage ROIs between napari and OMERO.
 
-def _init(widget):
-    shape_load_button = PushButton(text="Load Annotations from OMERO")
-    widget.insert(1, shape_load_button)
-    widget.shape_load_button = shape_load_button
+    This widget handles both loading ROI from OMERO, as well as saving
+    napari annotations to OMERO as ROI.
+    """
+    omero_image_combobox = create_widget(label="OMERO Image", annotation=Image)
+    load_button = PushButton(text="Load Annotations from OMERO")
+    save_button = PushButton(text="Upload Annotations to OMERO")
 
-    @shape_load_button.clicked.connect
-    def _load_rois_from_omero():
+    @load_button.clicked.connect
+    def _load_rois_from_omero() -> None:
         viewer = napari.viewer.current_viewer()
-        image_layer = widget.omero_image.value
+        image_layer = omero_image_combobox.value
 
         if not image_layer or "omero" not in image_layer.metadata:
             show_info("No OMERO metadata found in selected layer.")
@@ -48,46 +51,32 @@ def _init(widget):
         if points_meta:
             viewer.add_points(points_coords, **points_meta)
 
+    @save_button.clicked.connect
+    def _save_rois_to_omero() -> None:
+        omero_image = omero_image_combobox.value
+        # check if 'omero' field is in metadata
+        if not omero_image or "omero" not in omero_image.metadata:
+            warnings.warn("No OMERO metadata found in selected layer.", stacklevel=2)
+            return
 
-@magic_factory(
-    omero_image={"label": "OMERO ROI Manager"},
-    call_button="Upload Annotations to OMERO",
-    widget_init=_init,
-)
-def save_rois_to_OMERO(omero_image: Image) -> None:
-    """Upload annotations for a chosen image to OMERO.
+        # assert that layer is 4D if it is a labels layer
+        if isinstance(omero_image, Labels) and omero_image.ndim != 4:
+            raise ValueError(
+                "Labels layer must be 4D (time, z, y, x) to be uploaded to OMERO."
+            )
 
-    Parameters
-    ----------
-    omero_image: Image
-        An image layer loaded from OMERO.
-        Layer metadata has stored OMERO image ID.
-        Annotations will be uploaded to the OMERO server as ROI for that image ID.
+        gateway = QGateWay()
+        image_id = omero_image.metadata["omero"]["@id"]
 
-    Returns
-    -------
-    None
-    """
-    # check if 'omero' field is in metadata
-    if "omero" not in omero_image.metadata:
-        warnings.warn("No OMERO metadata found in selected layer.", stacklevel=2)
-        return
-
-    # assert that layer is 4D if it is a labels layer
-    if isinstance(omero_image, Labels) and omero_image.ndim != 4:
-        raise ValueError(
-            "Labels layer must be 4D (time, z, y, x) to be uploaded to OMERO."
+        image_wrapper = lookup_obj(
+            gateway.conn, ProxyStringType("Image")(f"Image:{image_id}")
         )
 
-    gateway = QGateWay()
-    image_id = omero_image.metadata["omero"]["@id"]
+        viewer = napari.viewer.current_viewer()
+        save_rois(viewer=viewer, image=image_wrapper)
 
-    image_wrapper = lookup_obj(
-        gateway.conn, ProxyStringType("Image")(f"Image:{image_id}")
-    )
+        trg = image_wrapper.getName()
+        show_info(f"All annotation layers uploaded to OMERO image id {image_id}: {trg}")
 
-    viewer = napari.viewer.current_viewer()
-    save_rois(viewer=viewer, image=image_wrapper)
-
-    trg = image_wrapper.getName()
-    show_info(f"All annotation layers uploaded to OMERO image id {image_id}: {trg}")
+    container = Container(widgets=[omero_image_combobox, load_button, save_button])
+    return container
